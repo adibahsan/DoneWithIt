0462cb2a120babe80ff7ba50b857b94bca46ad26
Adib Ahsan Chowdhury <adibahsan@iut-dhaka.edu>
2020-06-16 14:22:36 +0600

    view local images

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   console.log("App Executed");
 
-  let x = 1244;
-  x.toString;
-
   return (
     <View style={styles.container}>
-      <Text>Hello From my App {x}</Text>
+      <Text>
+        Culpa irure excepteur sint occaecat consectetur excepteur Lorem
+        excepteur fugiat reprehenderit ipsum. Lorem adipisicing fugiat laboris
+        enim qui cillum minim mollit nisi qui ea. Nostrud anim fugiat labore ex
+        ea elit. Mollit proident esse nisi ut Lorem ea Lorem commodo incididunt
+        irure. Magna amet enim Lorem do ut laboris quis est mollit non excepteur
+        amet. Enim qui velit commodo sit.
+      </Text>
+      <Image source={require("./assets/icon.png")} />
     </View>
   );
 }
