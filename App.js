4d2bae9258405d162a8487cb06e65eb232e50a79
Adib Ahsan Chowdhury <adibahsan@iut-dhaka.edu>
2020-06-16 14:02:06 +0600

    text with number of lines

--- App.js
+++ App.js
@@ -4,12 +4,16 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   console.log("App Executed");
 
-  let x = 1244;
-  x.toString;
-
   return (
     <View style={styles.container}>
-      <Text>Hello From my App {x}</Text>
+      <Text numberOfLines={1} style={styles.colorStyles}>
+        Ullamco eiusmod laborum ad deserunt amet culpa amet dolor nulla
+        consectetur excepteur veniam. Lorem officia deserunt do ipsum Lorem. Eu
+        id officia quis consectetur. Pariatur anim fugiat labore minim
+        adipisicing consectetur exercitation deserunt enim. Do consequat aliquip
+        non exercitation aute voluptate id officia velit excepteur qui culpa in.
+        Officia ad deserunt qui reprehenderit nostrud culpa.
+      </Text>
     </View>
   );
 }
@@ -17,8 +21,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "dodgerblue",
     alignItems: "center",
     justifyContent: "center",
+  },
+
+  colorStyles: {
+    color: "#fff",
   },
 });
