193c9bd9d4fda9bdcee5118c5645f7af1a0f6cf6
Adib Ahsan Chowdhury <adibahsan@iut-dhaka.edu>
2020-06-16 14:33:52 +0600

    download images with touchables

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableHighlight,
+} from "react-native";
 
 export default function App() {
   console.log("App Executed");
+
+  const handlePress = () => {
+    console.log("image pressed");
+  };
 
   return (
     <View style={styles.container}>
@@ -14,7 +24,17 @@ export default function App() {
         irure. Magna amet enim Lorem do ut laboris quis est mollit non excepteur
         amet. Enim qui velit commodo sit.
       </Text>
-      <Image source={require("./assets/icon.png")} />
+
+      <TouchableHighlight onPress={handlePress}>
+        <Image
+          blurRadius={5}
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableHighlight>
     </View>
   );
 }
@@ -22,7 +42,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "dodgerblue",
+    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
